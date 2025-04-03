ac6061fc1b6c78f7ea6fc06d0d309f04cb356564
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-04-03 23:13:24 +0530

    Respond with body

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,30 @@
-#[allow(unused_imports)]
 use std::{
     io::{BufRead, BufReader, Write},
-    net::TcpListener,
+    net::{TcpListener, TcpStream},
 };
+
+fn handle_request(mut _stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut _stream);
+    let request = buf_reader.lines().next().unwrap().unwrap();
+
+    // let request_type = request.split(" ").nth(0).unwrap();
+    let request_path = request.split(" ").nth(1).unwrap();
+
+    let res = if request_path == "/" {
+        "HTTP/1.1 200 OK\r\n\r\n".to_string()
+    } else if request_path.starts_with("/echo") {
+        let echo_str = request_path.trim_start_matches("/echo/");
+        format!(
+            "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
+            echo_str.len(),
+            echo_str
+        )
+        .to_string()
+    } else {
+        "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+    };
+    _stream.write_all(res.as_bytes()).unwrap();
+}
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -10,13 +32,7 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                let buf_reader = BufReader::new(&mut _stream);
-                let req = buf_reader.lines().next().unwrap().unwrap();
-                let res = match req.as_str() {
-                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
-                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
-                };
-                _stream.write_all(res.as_bytes()).unwrap()
+                handle_request(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
