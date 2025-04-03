915baf778eda9ad0ca31f4e2af92a0fb20be1324
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-04-03 22:10:33 +0530

    Extract URL path

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 #[allow(unused_imports)]
-use std::{io::Write, net::TcpListener};
+use std::{
+    io::{BufRead, BufReader, Write},
+    net::TcpListener,
+};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
@@ -7,8 +10,13 @@ fn main() {
     for stream in listener.incoming() {
         match stream {
             Ok(mut _stream) => {
-                println!("accepted new connection");
-                _stream.write_all("HTTP/1.1 200 OK\r\n\r\n".as_bytes()).unwrap()
+                let buf_reader = BufReader::new(&mut _stream);
+                let req = buf_reader.lines().next().unwrap().unwrap();
+                let res = match req.as_str() {
+                    "GET / HTTP/1.1" => "HTTP/1.1 200 OK\r\n\r\n".to_string(),
+                    _ => "HTTP/1.1 404 Not Found\r\n\r\n".to_string(),
+                };
+                _stream.write_all(res.as_bytes()).unwrap()
             }
             Err(e) => {
                 println!("error: {}", e);
