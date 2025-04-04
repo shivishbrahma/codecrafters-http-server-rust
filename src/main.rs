eb3de2f304968e501b9dbc2883cd5272ff85cf82
Purbayan Chowdhury <pur.cho.99@gmail.com>
2025-04-04 12:08:53 +0530

    Restructure the code

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,47 @@ use std::{
     net::{TcpListener, TcpStream},
 };
 
+enum HTTPStatus {
+    Ok,
+    NotFound,
+    MethodNotAllowed,
+}
+
+enum HTTPContentType {
+    TextPlain,
+    // TextHtml,
+}
+
+fn build_response(
+    http_status: HTTPStatus,
+    contents: String,
+    http_content_type: HTTPContentType,
+) -> String {
+    let length = contents.len();
+    let status = match http_status {
+        HTTPStatus::Ok => "200 OK",
+        HTTPStatus::NotFound => "404 Not Found",
+        HTTPStatus::MethodNotAllowed => "405 Method Not Allowed",
+    };
+    let content_type = match http_content_type {
+        HTTPContentType::TextPlain => "text/plain",
+        // HTTPContentType::TextHtml => "text/html",
+    };
+
+    format!(
+        "HTTP/1.1 {}\r\n{}",
+        status,
+        if length > 0 {
+            format!(
+                "Content-Type: {}\r\nContent-Length: {}\r\n\r\n{}",
+                content_type, length, contents
+            )
+        } else {
+            "\r\n".to_string()
+        }
+    )
+}
+
 fn handle_request(mut _stream: TcpStream) {
     let buf_reader = BufReader::new(&mut _stream);
     let mut lines = buf_reader.lines();
@@ -12,43 +53,47 @@ fn handle_request(mut _stream: TcpStream) {
     let request_type = request.split(" ").nth(0).unwrap();
     let request_path = request.split(" ").nth(1).unwrap();
 
-    let mut headers = HashMap::new();
-    for line in lines {
-        let line = line.unwrap();
-        if line.trim() != "" {
-            let parts: Vec<&str> = line.split(": ").collect();
-            let key = parts[0].to_string();
-            let value = parts[1].to_string();
-            headers.insert(key, value);
-        } else {
-            break;
-        }
-    }
+    let headers = lines
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .map(|line| {
+            (
+                line.split(": ").nth(0).unwrap().to_string(),
+                line.split(": ").nth(1).unwrap().to_string(),
+            )
+        })
+        .collect::<HashMap<String, String>>();
 
     let response = if request_type == "GET" {
         if request_path == "/" {
-            "HTTP/1.1 200 OK\r\n\r\n".to_string()
+            build_response(HTTPStatus::Ok, "".to_string(), HTTPContentType::TextPlain)
         } else if request_path.starts_with("/echo") {
             let echo_str = request_path.trim_start_matches("/echo/");
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                echo_str.len(),
-                echo_str
+            build_response(
+                HTTPStatus::Ok,
+                echo_str.to_string(),
+                HTTPContentType::TextPlain,
             )
-            .to_string()
         } else if request_path.starts_with("/user-agent") {
             let user_agent = headers.get("User-Agent").unwrap();
-            format!(
-                "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: {}\r\n\r\n{}",
-                user_agent.len(),
-                user_agent
+            build_response(
+                HTTPStatus::Ok,
+                user_agent.to_string(),
+                HTTPContentType::TextPlain,
             )
-            .to_string()
         } else {
-            "HTTP/1.1 404 Not Found\r\n\r\n".to_string()
+            build_response(
+                HTTPStatus::NotFound,
+                "".to_string(),
+                HTTPContentType::TextPlain,
+            )
         }
     } else {
-        "HTTP/1.1 405 Method Not Allowed\r\n\r\n".to_string()
+        build_response(
+            HTTPStatus::MethodNotAllowed,
+            "".to_string(),
+            HTTPContentType::TextPlain,
+        )
     };
     _stream.write_all(response.as_bytes()).unwrap();
 }
@@ -60,9 +105,7 @@ fn main() {
         match stream {
             Ok(mut _stream) => {
                 // handle_request(_stream);
-                std::thread::spawn(move || {
-                    handle_request(_stream);
-                });
+                std::thread::spawn(move || handle_request(_stream));
             }
             Err(e) => {
                 println!("error: {}", e);
